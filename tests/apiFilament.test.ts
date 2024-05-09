b2d38c4eecc12ca1ac5a0b225f46077cba759e41
Leevi Koskinen <31744881+leba9999@users.noreply.github.com>
2024-05-09 22:26:38 +0300

    test

--- tests/apiFilament.test.ts
+++ tests/apiFilament.test.ts
@@ -14,6 +14,11 @@ import { ErrorMessageResponse } from "../src/interfaces/responses";
 describe("Testing user interactions with API filament storage", () => {
   beforeAll(async () => {
     logger.silent = false; // turn off logging
+    console.log(process.env.MONGO_HOST);
+    console.log(process.env.MONGO_PORT);
+    console.log(process.env.MONGO_DB);
+    console.log(process.env.MONGO_USER);
+    console.log(process.env.MONGO_PASS);
     await MongoDataSource.initialize();
   }, 10000);
   afterAll(async () => {
